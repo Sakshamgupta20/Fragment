8ca7d863f2a87c5a362bd4a23db0c51ba6e6cf5c
Saksham <sg20saksham@gmail.com>
2018-04-11 22:14:58 +0530

    Studying Fragment Life Cycle

--- app/src/main/java/com/example/saksham/fragment/First.java
+++ app/src/main/java/com/example/saksham/fragment/First.java
@@ -1,11 +1,13 @@
 package com.example.saksham.fragment;
 
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,7 +20,7 @@ public class First extends Fragment {
                              Bundle savedInstanceState) {
         final View rootView = inflater.inflate(R.layout.activity_first, container, false);
 
-
+        Log.i("A","on createview" );
         Button button=(Button)rootView.findViewById(R.id.button1);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,4 +31,101 @@ public class First extends Fragment {
         });
     return rootView;
     }
+    @Override
+
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+
+        Log.i("A","on attach" );
+
     }
+    @Override
+
+    public void onCreate( Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        Log.i("A","on create" );
+
+    }
+
+
+    @Override
+
+    public void onActivityCreated( Bundle savedInstanceState) {
+
+        Log.i("A","on Activitycreated" );
+
+        super.onActivityCreated(savedInstanceState);
+
+    }
+
+    @Override
+
+    public void onStart() {
+
+        Log.i("A","on Start" );
+
+        super.onStart();
+
+    }
+
+    @Override
+
+    public void onResume() {
+
+        Log.i("A","on Resume" );
+
+        super.onResume();
+
+    }
+
+    @Override
+
+    public void onPause() {
+
+        Log.i("A","on Pause" );
+
+        super.onPause();
+
+    }
+    @Override
+
+    public void onStop() {
+
+        Log.i("A","on Stop" );
+
+        super.onStop();
+
+    }
+
+    @Override
+
+    public void onDestroy() {
+
+        super.onDestroy();
+
+        Log.i("A","on destroy" );
+
+    }
+
+
+    @Override
+
+    public void onDestroyView() {
+
+        super.onDestroyView();
+
+        Log.i("A","on Destroyview" );
+
+    }
+
+    @Override
+
+    public void onDetach() {
+
+        super.onDetach();
+
+        Log.i("A","on Detach" );
+
+    }}
--- app/src/main/java/com/example/saksham/fragment/MainActivity.java
+++ app/src/main/java/com/example/saksham/fragment/MainActivity.java
@@ -4,6 +4,7 @@ package com.example.saksham.fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
    static FragmentManager manager;
@@ -11,9 +12,60 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Log.i("B","on create Activity" );
         manager =getSupportFragmentManager();
         First first=new First();
         manager.beginTransaction().add(R.id.main,first).commit();
+
     }
-}
+
+    @Override
+
+    protected void onStart() {
+
+        super.onStart();
+
+        Log.i("B","on start Activity" );
+
+    }
+
+    @Override
+
+    protected void onResume() {
+
+        super.onResume();
+
+        Log.i("B","on resume Activity" );
+
+    }
+
+
+    @Override
+
+    protected void onPause() {
+
+        super.onPause();
+
+        Log.i("B","on pause Activity" );
+
+    }
+
+    @Override
+
+    protected void onStop() {
+
+        super.onStop();
+
+        Log.i("B","on stop Activity" );
+
+    }
+
+    @Override
+
+    protected void onDestroy() {
+
+        super.onDestroy();
+
+        Log.i("B","on destroy Activity" );
+
+    }}
